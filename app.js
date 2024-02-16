fc724b7f87c630ba3158884a81c391d9ea2197e7
jhogue84 <jhorguma@misena.edu.co>
2024-02-16 15:28:33 -0500

    primera carga de todos los archivos

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const servidor = express();
+
+puerto = 3000
+
+//configuracion ruta statica de public
+servidor.use(express.static(__dirname + "/public"))
+
+//motor de plantillas
+servidor.set("view engine", "ejs")
+servidor.set("views", __dirname + "/views")
+
+//rutas
+servidor.get("/", (req, res)=>{
+    //res.send("Respuesta del servidor en raiz")
+    res.render("index", {titulo: "Bienvenidos index con plantilla ejs"})
+})
+
+servidor.get("/nosotros", (req, res)=>{
+    //res.send("Respuesta de pagina nosotros")
+    res.render("nosotros", {titulo: "Pagina de Nosotros"})
+})
+
+servidor.use((req, res, next)=>{
+    //res.status(404).sendFile(__dirname + "/public/404.html")
+    res.status(404).render("404")
+
+})
+
+//escucha
+servidor.listen(puerto, ()=>{
+    console.log("escuchando desde el puerto:", puerto)
+})
